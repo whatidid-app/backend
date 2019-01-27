84c4e983d647e5400290de643f950b61f3c82ff6
Josh Hamilton <nearbycoder@gmail.com>
2019-01-27 15:10:16 -0600

    test cleanup

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,13 +1,10 @@
-module Types
-  class QueryType < Types::BaseObject
-    # Add root-level fields here.
-    # They will be entry points for queries on your schema.
+# frozen_string_literal: true
 
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World!"
-    end
+class Types::QueryType < Types::BaseObject
+  # TODO: remove me
+  field :test_field, String, null: false,
+                             description: 'An example field added by the generator'
+  def test_field
+    'Hello World!'
   end
 end
