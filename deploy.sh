617aece873e832dc42437ec28bb54bf0c721f60b
Josh Hamilton <nearbycoder@gmail.com>
2019-02-02 17:14:19 -0600

    run migration

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,4 @@
 git stash --all
 git checkout master
 git push -f deploy master
-ssh dokku@206.189.200.49
-dokku run api bundle exec rake db:migrate
+ssh dokku@206.189.200.49 run api bundle exec rake db:migrate
